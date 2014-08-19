b2d1b7c59d4870a5b82fb1a8fe29646986c3c623
sakabe <sakabe.kodai@tis.co.jp>
2014-08-19 10:39:15 +0900

    modified nginx

---
+++ site-cookbooks/my-nginx/attributes/community.rb
@@ -0,0 +1,20 @@
+#Comminuty nginx recipe
+default['nginx']['dir']                 = '/etc/nginx'
+default['nginx']['log_dir']             = '/var/log/nginx'
+default['nginx']['user']                = 'nginx'
+default['nginx']['pid']                 = '/var/run/nginx.pid'
+
+case node['platform_family']
+when 'rhel', 'fedora'
+  case node['platform']
+  when 'centos'
+    # See http://wiki.nginx.org/Install
+    default['nginx']['upstream_repository'] = "http://nginx.org/packages/centos/#{node['platform_version'].to_i}/$basearch/"
+  when 'amazon'
+    default['nginx']['upstream_repository'] = "http://nginx.org/packages/rhel/6/$basearch/"
+  else
+    default['nginx']['upstream_repository'] = "http://nginx.org/packages/rhel/#{node['platform_version'].to_i}/$basearch/"
+  end
+when 'debian'
+  default['nginx']['upstream_repository'] = "http://nginx.org/packages/#{node['platform']}"
+end
--- site-cookbooks/my-nginx/recipes/default.rb
+++ site-cookbooks/my-nginx/recipes/default.rb
@@ -7,29 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 
-if platform_family?('rhel')
-  if node['nginx']['repo_source'] == 'epel'
-    include_recipe 'yum-epel'
-  elsif node['nginx']['repo_source'] == 'nginx'
-    include_recipe 'nginx::repo'
-    package_install_opts = '--disablerepo=* --enablerepo=nginx'
-  elsif node['nginx']['repo_source'].nil?
-    log "node['nginx']['repo_source'] was not set, no additional yum repositories will be installed." do
-      level :debug
-    end
-  else
-    fail ArgumentError, "Unknown value '#{node['nginx']['repo_source']}' was passed to the nginx cookbook."
-  end
-elsif platform_family?('debian')
-  include_recipe 'nginx::repo' if node['nginx']['repo_source'] == 'nginx'
-end
+include_recipe 'nginx::repo'
 
-package node['nginx']['package_name'] do
-  options package_install_opts
-  notifies :reload, 'ohai[reload_nginx]', :immediately
-  not_if 'which nginx'
+package "nginx" do
+  action :install
 end
-
 
 service "nginx" do
   action [ :enable, :start ]
