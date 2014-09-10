2f2945a670d09d2c646fcf9154871200c61e54c3
koudaiii <cs006061@gmail.com>
2014-09-10 23:36:22 +0900

    add recipe rbenv-default-gems in my_rbenv.

--- site-cookbooks/my_rbenv/attributes/default.rb
+++ site-cookbooks/my_rbenv/attributes/default.rb
@@ -1,5 +1,6 @@
-default['my_rbenv']['user']            = "rbenv"
-default['my_rbenv']['group']           = "rbenv"
+default['my_rbenv']['user']            = 'rbenv'
+default['my_rbenv']['group']           = 'rbenv'
 default['my_rbenv']['version']         = '2.1.2'
 default['my_rbenv']['my_rbenv_url']    = 'https://github.com/sstephenson/rbenv.git'
 default['my_rbenv']['ruby-build_url']  = 'https://github.com/sstephenson/ruby-build.git'
+default['my_rbenv']['rbenv-default-gems']  = 'https://github.com/sstephenson/rbenv-default-gems.git'
--- site-cookbooks/my_rbenv/recipes/default.rb
+++ site-cookbooks/my_rbenv/recipes/default.rb
@@ -63,33 +63,30 @@ when "ubuntu", "debian"
   package "libxml2-dev"
 end
 
-#TODO
-#directory "/usr/local/rbenv" do
-#  owner node["my_rbenv"]["user"]
-#  group node["my_rbenv"]["group"]
-#  mode 0755
-#  action :create
-#end
 
 git "/usr/local/rbenv" do
   repository node["my_rbenv"]["my_rbenv_url"]
   action :sync
-#  user node["my_rbenv"]["user"]
-#  group node["my_rbenv"]["group"]
 end
 
-#TODO
 %w{plugins shims versions}.each do |dir|
   directory "/usr/local/rbenv/#{dir}" do
     owner "root"
     group "root"
-    mode 0777
+    mode 0755
     action :create
   end
 end
 
 git "/usr/local/rbenv/plugins/ruby-build" do
   repository node["my_rbenv"]["ruby-build_url"]
+  action :sync
+  user "root"
+  group "root"
+end
+
+git "/usr/local/rbenv/plugins/rbenv-default-gems" do
+  repository node["my_rbenv"]["rbenv-default-gems"]
   action :sync
   user "root"
   group "root"
@@ -102,6 +99,15 @@ template "rbenv.sh" do
   owner "root"
   group "root"
   not_if "grep rbenv /etc/profile.d/rbenv.sh"
+end
+
+template "default-gems" do
+  source "default-gems.erb"
+  path "/usr/local/rbenv/default-gems"
+  mode 0755
+  owner "root"
+  group "root"
+  not_if { File.exists?("/usr/local/rbenv/default-gems") }
 end
 
 include_recipe 'my_rbenv::ruby_install'
