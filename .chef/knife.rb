9d69fc39a224ac6d395ec8678d0fda365404f7c6
sakabe <sakabe.kodai@tis.co.jp>
2014-09-30 20:37:32 +0900

    Setting chef-zero

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -6,3 +6,9 @@ data_bag_path    "data_bags"
 #encrypted_data_bag_secret "data_bag_key"
 
 knife[:berkshelf_path] = "cookbooks"
+chef_server_url "http://127.0.0.1:8889"
+node_name       "stickywicket"
+client_key      File.join(File.dirname(__FILE__), "stickywicket.pem")
+validation_key File.join(File.dirname(__FILE__), "stickywicket.pem")
+cache_type      "BasicFile"
+cache_options   :path => "#{chef_repo}/checksums"
